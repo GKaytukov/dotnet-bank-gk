2e76fd69cb10a6a79d054e62515d62c5c6effa1b
Gerly Kaytukov <gerlyecc@yahoo.com>
2022-11-14 14:43:33 -0500

    updating code

--- program.cs
+++ program.cs
@@ -1,1 +1,20 @@
-Console.WriteLine("hello world");
+Console.WriteLine("Welcome to the bank");
+
+BankAccount account = new BankAccount(11000);
+//BankAccount account = new BankAccount("new account name"); it will know to use the string version under BankAccount 
+
+//account.balance = 100;
+try{ 
+account.makeDeposit(-1);
+} catch(Exception ex) {
+    Console.WriteLine(ex.Message); 
+}
+
+Console.Write($" Your balance is: account.balance {account.getBalance()} ");
+Console.WriteLine($" Account Number: {account.accountNumber}");
+
+//public string accountNumber {
+//   public string accountNumber {get; set;} ="";
+//   public decimal balance {get; set;} ="";
+//}
+ 
